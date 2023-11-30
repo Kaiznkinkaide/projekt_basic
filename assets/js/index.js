0079d5b7be45a83c99ca89a0637d87b7f4a9f2e8
digitalflowerstudio <floriantappeser@gmail.com>
2023-11-30 10:27:08 +0100

    added smooth scrolling, connected css and js

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
